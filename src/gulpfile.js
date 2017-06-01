8fe6c13f2c09c99729a0c7cdce465906da85ba7e
alexey-golovin <alexey.golovin@abz.agency>
2017-06-01 22:14:01 +0300

    landing page added

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -6,12 +6,21 @@ var gulp        = require('gulp'),
     csso        = require('gulp-csso'),
     concat      = require('gulp-concat'),
     minifyjs    = require('gulp-minify'),
-    processhtml = require('gulp-processhtml');
+    processhtml = require('gulp-processhtml'),
+    connect     = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  });
+});
 
 gulp.task('sass', function () {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('css_min_concat', function () {
@@ -38,7 +47,22 @@ gulp.task('js_min_concat', function () {
 gulp.task('posthtml', function () {
     return gulp.src('./*.html')
         .pipe(processhtml())
-        .pipe(gulp.dest('../views'));
+        .pipe(gulp.dest('../views'))
 });
 
+gulp.task('html', function () {
+  gulp.src(['./*.html', './**/*.html'])
+    .pipe(connect.reload());
+});
+
+gulp.task('html:watch', function () {
+  gulp.watch(['./*.html', './**/*.html'], ['html']);
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+
 gulp.task('prod', ['sass', 'css_min_concat', 'js_min_concat', 'posthtml'], function() {});
+gulp.task('watch',  ['connect', 'html:watch', 'sass:watch']);
---
+++ src/js/landing.js
@@ -0,0 +1,28 @@
+/*
+Copyright (c) 2016-2017 The Nirdizati Project.
+This file is part of "Nirdizati".
+
+"Nirdizati" is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 3 of the
+License, or (at your option) any later version.
+
+"Nirdizati" is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this program.
+If not, see <http://www.gnu.org/licenses/lgpl.html>.
+*/
+
+'use strict';
+
+$(document).ready(function() {
+	$('#js-learn-more').on('click', function() {
+		$('html, body').animate({
+			scrollTop: window.innerHeight
+		}, 400);
+	})
+});
